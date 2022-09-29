0c502b1bea5fe9ddc9a351a5be6c50cb769876d4
Sahil <sahilpathania682@gmail.com>
2022-09-29 14:50:58 +0530

    Initial Commit

---
+++ contact.php
@@ -0,0 +1,63 @@
+<?php include_once("includes/header.php"); ?> 
+	<div class="crumb">
+    	<p></p>
+        <ul>
+        	<li class="first"><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+    <div class="clear"></div>
+	<div id="content_sec">
+		<div class="col1">
+			<div class="contact">
+				<h4 class="heading colr">Any Review For Us</h4>
+				<form action="contact-confirmation.php">
+					<ul class="forms">
+						<li class="txt">Name</li>
+						<li class="inputfield"><input name="in" type="text" class="bar" required/></li>
+					</ul>
+					<ul class="forms">
+						<li class="txt">Company</li>
+						<li class="inputfield"><input name="in" type="text" class="bar" required/></li>
+					</ul>
+					<ul class="forms">
+						<li class="txt">Email</li>
+						<li class="inputfield"><input name="in" type="text" class="bar" required/></li>
+					</ul>
+					<ul class="forms">
+						<li class="txt">Phone</li>
+						<li class="inputfield"><input name="in" type="text" class="bar" required/></li>
+					</ul>
+					<ul class="forms">
+						<li class="txt">Message</li>
+						<li class="textfield"><textarea name="in" cols="" rows="6" required></textarea></li>
+					</ul>
+					<div class="clear"></div>
+					<ul class="forms">
+						<li class="txt">&nbsp;</li>
+						<li class="textfield"><input type="submit" value="Submit" class="simplebtn"></li>
+						<li class="textfield"><input type="reset" value="Reset" class="resetbtn"></li>
+					</ul>
+				</form>
+			</div>
+		</div>
+		<div class="col2">
+			<div class="contactfinder">
+				<h4 class="heading colr">Where to find us.</h4>
+				<a href="#" class="mapcont"><img src="./images/map.gif" alt="" style="width:250px;"/></a>
+				<h4></h4>
+				<p>
+					
+					<br />
+					<br />
+					<br />
+					<br />
+					<br />
+					<br /><br />
+					+<br />
+					<a href=""></a><br />
+				</p>
+			</div>
+		</div>
+	</div>
+<?php include_once("includes/footer.php"); ?> 
---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<div class="clear"></div>
+<div id="footer">
+    <ul class="">
+    	 
+			<p style="font-size:35px;color:white;padding-left:50px;padding-top:15px;text-align:left;"><b>Freshfarm</b></p>
+			<br>
+			<p style="padding-left:50px;text-align:left;color:white;" ><b>Address:</b>Guru Nanak Dev University Amritsar</p>
+				<p style="padding-left:50px;text-align:left;color:white;"><b>Email:</b> sahilpathania682@gmail.com</p>
+					<p style="padding-left:50px;text-align:left;color:white;"><b>Call Us:</b>(8091005937)</p>	
+					</p>
+		
+    </ul>
+    <div class="clear"></div>
+</div>
+</body>
+</html>
